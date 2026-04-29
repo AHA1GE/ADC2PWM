91636a1704b11aed72a3483f5a16cfe634e009d2
Harry Sun <syfsyf@me.com>
2026-04-29 17:22:06 +0800

    plan support for PC4 / TIM1_CH4

--- ADC2PWM/src/ch32/ServoTimers.h
+++ ADC2PWM/src/ch32/ServoTimers.h
@@ -4,14 +4,16 @@
 /*
  * Minimal CH32 timer definitions for this Servo port.
  *
- * The CH32 backend intentionally supports only two virtual pins:
- *   SERVO_PIN_CH1 -> PD4 / TIM2_CH1
- *   SERVO_PIN_CH4 -> PD7 / TIM2_CH4
+ * The CH32 backend intentionally supports only below pins:
+ *   PD4_TIM2CH1 -> PD4 / TIM2_CH1
+ *   PD7_TIM2CH4 -> PD7 / TIM2_CH4
+ *   PC4_TIM1CH4 -> PC4 / TIM1_CH4
  */
 #define _useTimer2
 typedef enum { _timer2, _Nbr_16timers } timer16_Sequence_t;
 
-#define SERVO_PIN_CH1 0
-#define SERVO_PIN_CH4 1
+#define PD4_TIM2CH1 0 // PD4 / TIM2_CH1
+#define PD7_TIM2CH4 1 // PD7 / TIM2_CH4
+#define PC4_TIM1CH4 2 // PC4 / TIM1_CH4
 
 #endif
