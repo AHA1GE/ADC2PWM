6195b6f240da094143e74736436a44cb354f3825
Harry Sun <syfsyf@me.com>
2026-05-20 14:35:40 +0800

    perfect

--- Firmware_CH32V003/TEST/TEST.ino
+++ Firmware_CH32V003/TEST/TEST.ino
@@ -1,17 +1,21 @@
 #include "CH32V003_SERVO.h"
 
-
 #define PWM_PIN SERVO_PIN_PC4_TIM1_CH4
-
-#define PWM_BOOT_US 890
 
 static Servo esc;
 
+static uint16_t adcToPulseUs(uint16_t adcValue) {
+	return (uint16_t)map((long)adcValue, 0L, (long)1023, 1000, 2000);
+}
 
 void setup() {
     esc.attach(PWM_PIN);
-	esc.writeMicroseconds(PWM_BOOT_US);
+	esc.writeMicroseconds(890);
+    while (analogRead(PA2) > 500 ){
+        continue;
+    }
 }
 
 void loop() {
+    esc.writeMicroseconds(adcToPulseUs(analogRead(PA2)));
 }
